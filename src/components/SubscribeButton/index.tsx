549c294786be5d30894af5881f11c01594a54140
evelynhelena <evelynhelena781@gmail.com>
2022-03-07 20:17:59 -0300

    Arrumando bug de css

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,23 @@
 import Button from '@mui/material/Button';
-import styles from './subscribeButton.module.scss';
 export function SubscribeButton() {
     return (
         <Button
             variant="contained"
-            className={styles.subscribeButton}
+            sx={
+                {
+                    marginTop: 3,
+                    width: "18.75rem",
+                    fontSize: "1.25rem",
+                    height: "5rem",
+                    fontWeight: "bold",
+                    backgroundColor: "var(--purple-100)",
+                    ":hover": {
+                        backgroundColor: "var(--cyan-100)",
+                        boxShadow: "0px 6px 11px -2px rgba(78, 160, 171)",
+                    }
+                }
+            }
+
         >
             Inscreva-se agora
         </Button>
